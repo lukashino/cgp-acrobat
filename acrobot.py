27d7bdb885bcd45fc24cae6847eb20350034891d
Lukas Sismis <lukas.sismis@gmail.com>
2023-06-14 12:32:12 +0200

    OOP design revisited

--- acrobot.py
+++ acrobot.py
@@ -2,29 +2,51 @@ import cgp
 import gymnasium as gym
 import numpy as np
 import sympy
-from typing import Callable, List, Dict, Optional, Any
+from typing import Callable, List
 import argparse
+import time as t
+
+class ILogCGPIndividualRun:
+    def __init__():
+        pass
+
+    def log(self, ind_run_id: int, ind_id: int, ttc_sec: float, stc: float):
+        pass
+
+class ILogCGPIndividual:
+    def __init__():
+        pass
+
+    def log(self, ind_id: int, evol_id: int, ttc_sec: float):
+        pass
 
 
+class CSVLoggerCGPIndividualRun(ILogCGPIndividualRun):
+    def __init__(self, fname: str):
+        self.fname = fname
+        with open(self.fname, "w") as f:
+            f.write(f"ind_run_id,ind_id,ttc_sec,stc\n",)
 
-class CGPAgent:
-    def __init__(self, seed=8483992, individual_runs_cnt=30, individual_run_steps_cnt=200, render=False, 
-                 population_params=None, genome_params=None, ea_params=None, evolve_params=None):
-        self.seed = seed
-        self.render = render
-        self.individual_runs_cnt = individual_runs_cnt
-        self.individual_run_steps_cnt = individual_run_steps_cnt
-        self.population_params = population_params
-        self.genome_params = genome_params
-        self.ea_params = ea_params
-        self.evolve_params = evolve_params
+    def log(self, ind_run_id: int, ind_id: int, ttc_sec: float, stc: float):
+        with open(self.fname, "a") as f:
+            f.write(f"{ind_run_id},{ind_id},{ttc_sec},{stc}\n")
 
-        self.history = {
-            "expr_champion": [],
-        }
+class CSVLoggerCGPIndividual(ILogCGPIndividual):
+    def __init__(self, fname: str):
+        self.fname = fname
+        with open(self.fname, "w") as f:
+            f.write(f"ind_id,evol_id,ttc_sec\n")
+    
+    def log(self, ind_id: int, evol_id: int, ttc_sec: float):
+        with open(self.fname, "a") as f:
+            f.write(f"{ind_id},{evol_id},{ttc_sec}\n")
 
-    def _recording_callback(self, pop: Any) -> None:
-        self.history["expr_champion"].append(pop.champion.to_sympy())
+class CGPIndividualEpisode:
+    def __init__(self, id: int, max_episode_steps: int, f: Callable, env: gym.Env) -> None:
+        self.id = id
+        self.f = f
+        self.env = env
+        self.max_episode_steps = max_episode_steps
 
     # on weird result we return neutral action
     def _sanitize_cgp_action(self, next_action: float) -> int:
@@ -35,58 +57,97 @@ class CGPAgent:
             return int(np.round(next_action) % 3)
         except ValueError:
             return 1
-        
-    # return True on successful end-reaching state
-    def _simulate_individual_episode(
-            self, f: Callable, env: Any = None) -> bool:
-        observation, _ = env.reset()
-        for _ in range(self.individual_run_steps_cnt):
-            next_action = self._sanitize_cgp_action(f(*observation))
+    
+    def simulate(self, seed: int = None) -> bool:
+        if seed is not None:
+            observation, _ = self.env.reset(seed=seed)
+        else:
+            observation, _ = self.env.reset()
+        res = False
+        for i in range(self.max_episode_steps):
+            next_action = self._sanitize_cgp_action(self.f(*observation))
             observation, _, terminated, truncated, _ = \
-                env.step(next_action)
-
-            if terminated:
-                return True
-            elif truncated:
-                return False
+                self.env.step(next_action)
             
-        return False
+            if terminated or truncated:
+                res = True if terminated else False
+                break
+            
+        return res
 
-    def _simulate_individual_runs(
-            self, f: Callable, render: bool = False) -> List[bool]:
-        env = gym.make('Acrobot-v1', render_mode="human" if render else None)
+class CGPIndividual:
+    def __init__(
+            self, id: int, f: Callable, episodes_cnt: int, max_episodes_steps: int, render: bool = False, env_seed: int = None) -> List[bool]:
+        self.id = id
+        self.f = f
+        self.human_render = render
+        self.episodes_cnt = episodes_cnt
+        self.max_episodes_steps = max_episodes_steps
+        self.env_seed = env_seed
 
+    def _fitness_get(self, episodes_rslts: List[bool]) -> float:
+        return float(len(episodes_rslts)) / self.episodes_cnt
+
+    def simulate_episodes(self):
         episodes_success: List[bool] = []
+        env = gym.make(
+            'Acrobot-v1', render_mode="human" if self.human_render else None)
 
-        for _ in range(self.individual_runs_cnt):
-            res = self._simulate_individual_episode(f, env)
+        for i in range(self.episodes_cnt):
+            ep = CGPIndividualEpisode(i, self.max_episodes_steps, self.f, env)
+            res = ep.simulate(self.env_seed)
             if res:
-                episodes_success.append(
-                    res
-                )
+                episodes_success.append(res)
         
         env.close()
-        return episodes_success
+        return self._fitness_get(episodes_success)
+    
 
-    def _objective(self, individual: Any) -> Any:
+
+class CGPAgent:
+    def __init__(self, seed=None, individual_runs_cnt=30, individual_run_steps_cnt=200, render=False, 
+                 population_params=None, genome_params=None, ea_params=None, evolve_params=None):
+        self.generation_id = 0
+        self.seed = seed
+        self.render = render
+        self.individual_runs_cnt = individual_runs_cnt
+        self.individual_run_steps_cnt = individual_run_steps_cnt
+        self.population_params = population_params
+        self.genome_params = genome_params
+        self.ea_params = ea_params
+        self.evolve_params = evolve_params
+
+        self.logger_ir: ILogCGPIndividualRun = CSVLoggerCGPIndividualRun("ir.csv")
+        self.logger_i: ILogCGPIndividual = CSVLoggerCGPIndividual("i.csv")
+
+        self.history = {
+            "expr_champion": [],
+            "node_champion": [],
+        }
+
+    def _recording_callback(self, pop: cgp.Population) -> None:
+        self.history["expr_champion"].append(pop.champion.to_sympy())
+        self.history["node_champion"].append(pop.champion)
+
+    def _objective(self, individual: cgp.individual) -> cgp.individual:
         if not individual.fitness_is_None():
             return individual
         
         f = individual.to_func()
+        ivdl = CGPIndividual(self.generation_id, f, self.individual_runs_cnt, self.individual_run_steps_cnt, render=self.render, env_seed=self.seed)
         try:
-            res = self._simulate_individual_runs(f, render=self.render)
+            individual.fitness = ivdl.simulate_episodes()
         except ZeroDivisionError:
-            res = []
-
-        if len(res) > 0:
-            individual.fitness = float(len(res)) / self.individual_runs_cnt
-        else:
             individual.fitness = -np.inf
 
+        self.generation_id += 1
         return individual
 
-
     def visualize_final_solution(self) -> None:
+        ind: cgp.individual = self.history["node_champion"][-1]
+        cg = cgp.CartesianGraph(ind.genome)
+        print(f"CG pretty print - {cg.pretty_str()}")
+
         expr = self.history["expr_champion"][-1]
         expr_str = str(expr)
 
@@ -98,7 +159,8 @@ class CGPAgent:
         def f(x,y,z,a,b,c):
             return f_lambdify(x,y,z,a,b,c)
         
-        self._simulate_individual_runs(f, render=True)
+        cgp_ind = CGPIndividual(0, f, self.individual_runs_cnt, self.individual_run_steps_cnt, render=True, env_seed=None)
+        cgp_ind.simulate_episodes()
 
     def evolve(self):
         pop = cgp.Population(**self.population_params, genome_params=self.genome_params)
@@ -110,7 +172,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='CGP Agent parameters.')
 
     # CGPAgent arguments
-    parser.add_argument('--seed', type=int, default=8483992, help='Seed value for the simulator agent.')
+    parser.add_argument('--seed', type=int, default=None, help='Seed value for the simulator agent.')
     parser.add_argument('--individual_runs_cnt', type=int, default=30, help='Individual runs count.')
     parser.add_argument('--individual_run_steps_cnt', type=int, default=200, help='Individual run steps count.')
     parser.add_argument('--render', type=bool, action=argparse.BooleanOptionalAction, default=False, help='Visualizing the whole learning process.')
@@ -125,7 +187,7 @@ if __name__ == "__main__":
     parser.add_argument('--n_processes', type=int, default=8, help='Number of processes.')
     parser.add_argument('--max_generations', type=int, default=1500, help='Max generations.')
     parser.add_argument('--termination_fitness', type=float, default=0.95, help='Termination fitness from 0 to 1.')
-    parser.add_argument('--visualize', type=bool, action=argparse.BooleanOptionalAction, default=False, help='Whether to visualize the final solution.')
+    parser.add_argument('--visualize', type=bool, action=argparse.BooleanOptionalAction, default=True, help='Whether to visualize the final solution.')
 
     args = parser.parse_args()
 
